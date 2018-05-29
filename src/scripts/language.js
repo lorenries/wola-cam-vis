9026d4e66700311c09e8988ce63f7ea39014f445
Loren Riesenfeld <lorenries@users.noreply.github.com>
2018-05-29 18:04:29 -0400

    spanish translation

---
+++ src/scripts/language.js
@@ -0,0 +1,4 @@
+var english = document.documentElement.lang === 'en';
+var spanish = document.documentElement.lang === 'es';
+
+export { english, spanish };
---
+++ src/scripts/translations.js
@@ -0,0 +1,20 @@
+var translations = {
+	fundingSource: {
+		eng: "Funding Source",
+		esp: "Fuente de Financiamiento"
+	},
+	description: {
+		eng: "Description",
+		esp: "Descripción"
+	},
+	amountUsDollars: {
+		eng: "Amount in US Dollars",
+		esp: "Cantidad en Dólares Estadounidenses"
+	},
+	readMore: {
+		eng: "Read More",
+		esp: "Leer Más"
+	}
+}
+
+export default translations;
