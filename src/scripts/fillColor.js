46b1e7b4394a11d66f600f6dc5bf3646218c7acd
Loren Riesenfeld <lorenries@users.noreply.github.com>
2018-05-29 18:06:13 -0400

    export colors

--- src/scripts/fillColor.js
+++ src/scripts/fillColor.js
@@ -1,8 +1,8 @@
 // Nice looking colors - no reason to buck the trend
 // @v4 scales now have a flattened naming scheme
-
+var colors = ['#404041', '#00B5EF', '#65BD60', '#DA1A32', '#F9C606'];
 var fillColor = d3.scaleOrdinal()
 .domain(['Borders & Counternarcotics', 'Development', 'Justice and Rule of Law', 'Law Enforcement', 'Violence Prevention'])
 .range(['#404041', '#00B5EF', '#65BD60', '#DA1A32', '#F9C606']);
 
-export {fillColor};
+export {fillColor, colors};
